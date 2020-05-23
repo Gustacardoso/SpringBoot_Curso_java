4713719e9619c3fd91b28b55dbad4c2b8ba52895
gustaCardoso <gustimba17@gmail.com>
2020-05-23 20:17:50 -0300

    OrderItem, many-to-many association with estra attributes

--- src/main/java/com/example/course/entities/Order.java
+++ src/main/java/com/example/course/entities/Order.java
@@ -2,6 +2,8 @@ package com.example.course.entities;
 
 import java.io.Serializable;
 import java.time.Instant;
+import java.util.HashSet;
+import java.util.Set;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
@@ -9,11 +11,11 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
 import javax.persistence.Table;
 
 import com.example.course.entities.enums.OrderStatus;
 import com.fasterxml.jackson.annotation.JsonFormat;
-import com.fasterxml.jackson.databind.jsonFormatVisitors.JsonFormatTypes;
 
 @Entity
 @Table(name = "tb_order")
@@ -30,6 +32,10 @@ public class Order implements Serializable {
 	@ManyToOne
 	@JoinColumn(name = "client_id")
 	private User client;
+	//tenho que colocar o id.order porque precisamos para cada order de pedido tem um id
+	@OneToMany(mappedBy = "id.order")
+	public Set<OrderItem> items = new HashSet<>();
+	
 	
 	public Order () {
 		
@@ -76,7 +82,9 @@ public class Order implements Serializable {
 	public void setClient(User client) {
 		this.client = client;
 	}
-
+   public Set<OrderItem> getItems(){
+	   return items;
+   }
 	@Override
 	public int hashCode() {
 		final int prime = 31;
